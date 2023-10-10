abfdbda683b8d2623e4cb87daeb7d4df193e7deb
Ayush Shrivastava <ayushstwt@gmail.com>
2023-10-11 00:08:01 +0530

    Day1

--- src/Day1.java
+++ src/Day1.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Day1 {
+/*
+# 30DaysOfCode
+
+# Challenges on Core Java
+
+# Day 1
+
+WAP the Code Using Only Using One System.out.println().
+
+Output :
+Hi
+Hello
+Bye
+
+ */
+
+
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String value1=scanner.next();
+        String value2=scanner.next();
+        String value3=scanner.next();
+        System.out.println(value1+"\n"+value2+"\n"+value3);
+    }
 }
